22d42aed7dd7f686b8cd86aaffeb8c76295bfde3
Chris <limadog2003@gmail.com>
2017-06-15 17:50:18 -0400

    Initial commit for admin page, with html, css, and js

---
+++ app/public/js/Admin.js
@@ -0,0 +1,18 @@
+
+var user = document.getElementById("user");
+var userPic = document.getElementById("userPic");
+var role = document.getElementById("role");
+var email = document.getElementById("email");
+var addButton = document.getElementById("addButton");
+
+addButton.onclick = function() {
+  alert("New member added");
+}
+
+user.onclick = function() {
+  //alert("clicked user button");
+}
+
+role.onclick = function() {
+  //alert("clicked on role");
+}
